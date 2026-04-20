8c2974c4330071202b57b2fb0938277435c18398
Daniel Sammartano <daniel.sammartano@outlook.com>
2026-04-20 20:25:28 -0300

    Adicionado Docker Compose

--- obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b8918f97e5d106f52600b0dda5031406dfe7601")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cc36edaf2c9c6ac13837c04f97b3ce508271aee")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
